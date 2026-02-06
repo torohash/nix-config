deb451bf00a729edcbe57440012223ecc060fe39
torohash <123091263+torohash@users.noreply.github.com>
2026-02-06 23:23:29 +0900

    add btop

--- nix/packages/common-store.nix
+++ nix/packages/common-store.nix
@@ -1,9 +1,10 @@
-{ buildEnv, htop, cloc, tmux, zellij, git, curl, websocat, jq, tree, unzip, bash-completion, chafa, ripgrep, xclip, nodejs, yazi, lazygit, terraform, terraform-ls, awscli2, ssm-session-manager-plugin }:
+{ buildEnv, htop, btop, cloc, tmux, zellij, git, curl, websocat, jq, tree, unzip, bash-completion, chafa, ripgrep, xclip, nodejs, yazi, lazygit, terraform, terraform-ls, awscli2, ssm-session-manager-plugin }:
 
 buildEnv {
   name = "common-store";
   paths = [
     htop
+    btop
     cloc
     tmux
     zellij
